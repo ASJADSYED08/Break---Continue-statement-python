7ce783b2de8ab10ab48bea2989e1d1d6873df4a2
ASJADSYED08 <hr49335@gmail.com>
2021-10-14 19:58:21 +0500

    first commit

---
+++ code.py
@@ -0,0 +1,19 @@
+i=0
+while(True):
+    if(i<5):
+        i=i+1
+        continue
+    print(i,end=" ")  
+    if(i==15): 
+        break
+    i=i+1
+
+"""# quiz by my logic
+while(True):
+    a=int(input("ENTER NUMBER \n"))
+    if(a<=100):
+        print("TRY AGAIN \n")
+        continue
+    if(a>100):
+        print("CONGRATS \n")
+        break"""    
